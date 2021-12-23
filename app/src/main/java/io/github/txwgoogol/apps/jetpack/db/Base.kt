2cf84f398b989974e79f99a0850d068323b3005a
txwgoogol <txwgoogol@gmail.com>
2021-12-23 16:27:57 +0800

    delete file

--- app/src/main/java/io/github/txwgoogol/apps/jetpack/db/Base.kt
+++
@@ -1,7 +0,0 @@
-package io.github.txwgoogol.apps.jetpack.db
-
-data class Base<T>(
-    val `data`: T,
-    val errorCode: Int,
-    val errorMsg: String
-)
--- app/src/main/java/io/github/txwgoogol/apps/jetpack/db/home/Tag.kt
+++
@@ -1,6 +0,0 @@
-package io.github.txwgoogol.apps.jetpack.db.home
-
-data class Tag(
-    val name: String,
-    val url: String
-)
